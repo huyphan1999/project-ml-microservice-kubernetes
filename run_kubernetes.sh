8f972ee6df6ca029976db0e9cdf48e246c215394
Huy Phan <phuhuy1999@gmail.com>
2022-08-25 17:15:19 +0000

    Run k8s and output

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=udacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run $dockerpath --image=vuachicken6/udacity:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $dockerpath 8000:80
